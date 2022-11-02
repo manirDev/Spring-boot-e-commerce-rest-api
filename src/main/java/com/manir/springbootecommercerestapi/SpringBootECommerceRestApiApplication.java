e33ebdcd3da169164091aad88e1e4bfe8c61d78f
manirDev <mhtnourmhtmjr@gmail.com>
2022-11-02 10:18:18 +0300

    spring boot e-commerce rest api

---
+++ src/main/java/com/manir/springbootecommercerestapi/SpringBootECommerceRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.manir.springbootecommercerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootECommerceRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootECommerceRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manir/springbootecommercerestapi/SpringBootECommerceRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manir.springbootecommercerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootECommerceRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
